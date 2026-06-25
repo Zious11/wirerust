65e535b3d1341dc4662ae6287c33eda8a0fe0cf6
Zious <jaredbrichards@gmail.com>
2026-06-25 08:11:33 -0500

    style(enip): cargo fmt -- sender_context array to rustfmt canonical form

--- src/analyzer/enip.rs
+++ src/analyzer/enip.rs
@@ -115,8 +115,7 @@ pub fn parse_enip_header(data: &[u8]) -> Option<EnipHeader> {
         session_handle: u32::from_le_bytes([data[4], data[5], data[6], data[7]]),
         status: u32::from_le_bytes([data[8], data[9], data[10], data[11]]),
         sender_context: [
-            data[12], data[13], data[14], data[15],
-            data[16], data[17], data[18], data[19],
+            data[12], data[13], data[14], data[15], data[16], data[17], data[18], data[19],
         ],
         options: u32::from_le_bytes([data[20], data[21], data[22], data[23]]),
     })
